446b5feac296abf75279b93693682be8a379edb1
Fahad Rahmani <rnfahad3@gmail.com>
2023-10-16 11:17:34 +0530

    code  changed

--- lib/ui_elements/blueGradient.dart
+++ lib/ui_elements/blueGradient.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 LinearGradient blueGradient = LinearGradient(
   end: Alignment.topCenter,
   colors: [
-    Color.fromARGB(255, 0, 179, 244),
+    Color.fromARGB(255, 1, 179, 244),
     Color.fromARGB(255, 4, 160, 160),
   ],
 );
